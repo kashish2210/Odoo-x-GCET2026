10befdf6cb24802c74179a81cea68078aeacb335
kashish shrivastav <en.shrivastavkashish@gmail.com>
2026-01-03 17:37:16 +0530

    adduup

--- accounts/models.py
+++ accounts/models.py
@@ -61,7 +61,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     role = models.CharField(
         max_length=20,
         choices=Role.choices,
-        default=Role.EMPLOYEE
+        default=Role.HR
     )
     
     is_active = models.BooleanField(default=True)
