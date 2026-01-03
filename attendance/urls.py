4a7d3b82ca993fcea7a539dde6c9330cbe0e7e12
Nothingspecial99 <122666701+Nothingspecial99@users.noreply.github.com>
2026-01-03 13:33:57 +0530

    point attendance nav-item to correct url

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import attendance
 
+app_name = "attendance"
+
 urlpatterns = [
-    path('', attendance)
+    path('', attendance, name='attendance')
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def attendance(request):
     return render(request, 'attendance/attendance.html')
